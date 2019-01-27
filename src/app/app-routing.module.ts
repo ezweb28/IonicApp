374ee8e8f0a03ee7f2818b4f85524c65da606ce8
ezweb28 <ezweb28@gmail.com>
2019-01-27 12:57:11 -0500

    HEROKU DEPLOYMENT.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './login/login.module#LoginPageModule' }
-
+  { path: '', redirectTo: 'movies', pathMatch: 'full'},
+  { path: 'home', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'movies', loadChildren: './pages/movies/movies.module#MoviesPageModule' },
+  { path: 'movies/:id', loadChildren: './pages/movie-details/movie-details.module#MovieDetailsPageModule' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' }
 ];
 @NgModule({
   imports: [
